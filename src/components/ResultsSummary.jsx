982327cefe44161ab3ea34ef64f25ea65f836133
jshapi4 <jshapi4@wgu.edu>
2026-05-03 14:59:10 -0700

    refactored with formatCurrency

--- src/components/ResultsSummary.jsx
+++ src/components/ResultsSummary.jsx
@@ -1,83 +1,56 @@
-function ResultsSummary({ results }) {
+import { formatCurrency } from "../utils/mortgageCalc";
+
+function ResultsSummary({ results, showTotals, setShowTotals }) {
   return (
     <>
-      <h2>Monthly Breakdown</h2>
+      <h2 className="title">Monthly Breakdown</h2>
       <section className="monthly-grid form-box">
         <div className="row-item">
           <span>Principal & Interest</span>
-          <strong>
-            {parseFloat(results.monthlyPandI).toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </strong>
+          <strong>{formatCurrency(results.monthlyPandI)}</strong>
         </div>
         <div className="row-item">
           <span>Property Taxes</span>
-          <strong>
-            {parseFloat(results.monthlyPropertyTax).toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </strong>
+          <strong>{formatCurrency(results.monthlyPropertyTax)}</strong>
         </div>
         {results.monthlyPMI > 0 && (
           <div className="row-item">
             <span>PMI ({(results.pmiRate * 100).toFixed(2)}%)</span>
-            <strong>
-              {parseFloat(results.monthlyPMI).toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}
-            </strong>
+            <strong>{formatCurrency(results.monthlyPMI)}</strong>
           </div>
         )}
         <div className="row-item total">
           <span>Monthly Total:</span>
-          <strong>
-            {parseFloat(results.monthlyTotal).toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </strong>
+          <strong>{formatCurrency(results.monthlyTotal)}</strong>
         </div>
       </section>
-
-      <h2>Totals</h2>
-      <section className="monthly-grid form-box">
-        <div className="row-item">
-          <span>Total Interest Paid</span>
-          {parseFloat(results.totalInterestPaid).toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD",
-          })}
-        </div>
-        <div className="row-item">
-          <span>Total Property Taxes Paid</span>
-          {parseFloat(results.totalPropertyTaxesPaid).toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD",
-          })}
-        </div>
-        <div className="row-item">
-          <span>Total Principal</span>
-          <strong>
-            {parseFloat(results.totalPrincipal).toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </strong>
-        </div>
-        <div className="row-item total">
-          <span>Total Amount Paid</span>
-          <strong>
-            {parseFloat(results.totalAmountPaid).toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </strong>
-        </div>
-      </section>
+      <button
+        className="totalsButton"
+        onClick={() => setShowTotals((prev) => !prev)}
+      >
+        {showTotals ? "Hide Totals" : "Show Totals"}
+      </button>
+      {showTotals && (
+        <section className="monthly-grid form-box">
+          <h2 className="title">Totals Over 30 Years</h2>
+          <div className="row-item">
+            <span>Total Interest Paid</span>
+            {formatCurrency(results.totalInterestPaid)}
+          </div>
+          <div className="row-item">
+            <span>Total Property Taxes Paid</span>
+            {formatCurrency(results.totalPropertyTaxesPaid)}
+          </div>
+          <div className="row-item">
+            <span>Total Principal</span>
+            <strong>{formatCurrency(results.totalPrincipal)}</strong>
+          </div>
+          <div className="row-item total">
+            <span>Total Amount Paid</span>
+            <strong>{formatCurrency(results.totalAmountPaid)}</strong>
+          </div>
+        </section>
+      )}
     </>
   );
 }
